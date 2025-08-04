d43594be21da5f75dc0b130eacc785151226d8bb
minji kim <yj6649yj@icomer.com>
2025-08-04 15:05:58 +0900

    fix: add alias and fix import path

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
-import Home from "@components/Home";
+
 import { RecoilRoot } from "recoil";
+import Home from "./components/Home";
 
 export default function App() {
   return (
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   base: "./",
   assetsInclude: ["**/*.glb", "**/*.gltf", "**/*.bin"],
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"), // 경로는 실제 폴더 위치에 맞게 수정
+    },
+  },
   build: {
     rollupOptions: {
       output: {
